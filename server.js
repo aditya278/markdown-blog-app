5110254062626a6b7ec174ea33c3e552944d79db
aditya278 <aditya.shukla278@gmail.com>
2020-10-13 20:52:59 +0530

    Routing to a dumm ejs page

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+//We'll write all views in ejs
+//then view engine will convert that ejs code to html
+app.set('view engine', 'ejs');
+
+
+//Tell our app to use the Article Router when the route is /articles
+app.use('/articles', articleRouter);
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const artiles = [{
+        title : 'Test Articles',
+        createdAt : Date.now(),
+        description : 'Test Description'
+    }]
+    res.render('index', { articles : artiles });
 })
 
 app.listen(PORT, () => {
