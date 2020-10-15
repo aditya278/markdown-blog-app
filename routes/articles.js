28fe9b452640fc760435e814079ffac075ad5b86
aditya278 <aditya.shukla278@gmail.com>
2020-10-15 11:49:04 +0530

    Added Edit page and edit buttons

--- routes/articles.js
+++ routes/articles.js
@@ -6,6 +6,11 @@ router.get('/new', (req, res) => {
     res.render('articles/new', { article : new Article() });
 })
 
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id);
+    res.render('articles/edit', { article : article });
+})
+
 router.get('/:slug', async (req, res) => {
     //Instead of finding by ID, we'll find by slug
     //const article = await Article.findById(req.params.id);
@@ -15,27 +20,38 @@ router.get('/:slug', async (req, res) => {
     res.render('articles/show', { article : article})
 })
 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title : req.body.title,
-        description : req.body.description,
-        markdown : req.body.markdown
-    });
-    try {
-        article = await article.save()
-        //Redirect to the slug instead of ID
-        //res.redirect(`/articles/${article.id}`);
-        res.redirect(`/articles/${article.slug}`);
-    }
-    catch(err) {
-        console.error(err);
-        res.render('articles/new', { article : article });
-    }
-})
+router.post('/', async (req, res, next) => {
+    req.article = new Article();
+    next();
+}, saveArticleAndRedirect('new'));
+
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+}, saveArticleAndRedirect('edit'))
 
 router.delete('/:id', async(req, res) => {
     await Article.findByIdAndDelete(req.params.id);
     res.redirect('/');
 })
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article;
+        article.title = req.body.title;
+        article.description = req.body.description;
+        article.markdown = req.body.markdown;
+        try {
+            article = await article.save()
+            //Redirect to the slug instead of ID
+            //res.redirect(`/articles/${article.id}`);
+            res.redirect(`/articles/${article.slug}`);
+        }
+        catch(err) {
+            console.error(err);
+            res.render(`articles/${path}`, { article : article });
+        }
+    }
+}
 
 module.exports = router;
